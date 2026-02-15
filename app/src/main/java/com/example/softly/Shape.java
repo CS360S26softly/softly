16b4c3d6e5796b91550a878c82c2190cade8112a
oozeman64 <27100046@lums.edu.pk>
2026-02-16 00:04:30 +0500

    first commit: Shape Class

---
+++ app/src/main/java/com/example/softly/Shape.java
@@ -0,0 +1,14 @@
+package com.example.softly;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
